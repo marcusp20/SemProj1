5260b0251ff81535b953ecea6b5d19a711393c17
marcus <marcp20@student.sdu.dk>
2020-09-23 14:03:22 +0200

    Re-Import WorldOfZuul framework
    The first import was from the auther's git, this framework is the handout from Blackboard.

--- WorldOfZuul/src/Command.java
+++ WorldOfZuul/src/Command.java
@@ -3,68 +3,46 @@
  * "World of Zuul" is a very simple, text based adventure game.  
  *
  * This class holds information about a command that was issued by the user.
- * A command currently consists of two strings: a command word and a second
- * word (for example, if the command was "take map", then the two strings
- * obviously are "take" and "map").
+ * A command currently consists of two parts: a CommandWord and a string
+ * (for example, if the command was "take map", then the two parts
+ * are TAKE and "map").
  * 
  * The way this is used is: Commands are already checked for being valid
  * command words. If the user entered an invalid command (a word that is not
- * known) then the command word is <null>.
+ * known) then the CommandWord is UNKNOWN.
  *
  * If the command had only one word, then the second word is <null>.
  * 
- * @author  Michael Kölling and David J. Barnes
- * @version 2011.07.31
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2006.03.30
  */
 
 public class Command
 {
-    private String commandWord;
+    private CommandWord commandWord;
     private String secondWord;
 
-    /**
-     * Create a command object. First and second word must be supplied, but
-     * either one (or both) can be null.
-     * @param firstWord The first word of the command. Null if the command
-     *                  was not recognised.
-     * @param secondWord The second word of the command.
-     */
-    public Command(String firstWord, String secondWord)
+    public Command(CommandWord commandWord, String secondWord)
     {
-        commandWord = firstWord;
+        this.commandWord = commandWord;
         this.secondWord = secondWord;
     }
 
-    /**
-     * Return the command word (the first word) of this command. If the
-     * command was not understood, the result is null.
-     * @return The command word.
-     */
-    public String getCommandWord()
+    public CommandWord getCommandWord()
     {
         return commandWord;
     }
 
-    /**
-     * @return The second word of this command. Returns null if there was no
-     * second word.
-     */
     public String getSecondWord()
     {
         return secondWord;
     }
 
-    /**
-     * @return true if this command was not understood.
-     */
     public boolean isUnknown()
     {
-        return (commandWord == null);
+        return (commandWord == CommandWord.UNKNOWN);
     }
 
-    /**
-     * @return true if the command has a second word.
-     */
     public boolean hasSecondWord()
     {
         return (secondWord != null);
