3fadcec6ae65788c03e8ce7a9d5a9abfec088635
marcus <marcp20@student.sdu.dk>
2020-09-23 14:14:18 +0200

    Add class documentation to WorldOfZuul

--- WorldOfZuul/src/WorldOfZuul.java
+++ WorldOfZuul/src/WorldOfZuul.java
@@ -1,3 +1,9 @@
+/**
+ * Class containing the main method, to launch the WorldOfZuul game.
+ *
+ * @author marcus pedersen
+ * @version 23-09-2020
+ */
 public class WorldOfZuul {
 
     public static void main(String[] args) {
