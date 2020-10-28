b84520aa73393fb46c2bafc9a022679d171a953e
Joachim Baumann <jobau19@student.sdu.dk>
2020-10-28 14:42:23 +0100

    Create Store

---
+++ WorldOfZuul/src/Store.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<Item> itemlist;
+
+    public Store() {
+        itemlist = new ArrayList<Item>();
+
+    }
+
+
+
+}
