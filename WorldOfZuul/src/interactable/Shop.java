b8b0b464c40cb0b6cf5bcf11a4a211c0ea9b5801
Dentstring <46445611+Dentstring@users.noreply.github.com>
2020-12-01 23:12:47 +0100

    Added break to loop;

--- WorldOfZuul/src/interactable/Shop.java
+++ WorldOfZuul/src/interactable/Shop.java
@@ -4,8 +4,8 @@ public class Shop extends Interactable {
     public void removeItem(String word)    {
         for(int i = 0; i < this.getCommandList().getItems().size(); i++ )   {
             if(this.getCommandList().getItems().get(i).equals("Buy " + word))    {
-                System.out.println("Removed");
                 this.getCommandList().getItems().remove(i);
+                break;
             }
         }
     }
