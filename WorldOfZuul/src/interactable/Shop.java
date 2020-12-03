bc37faadddd762aeec26ffd26e64cc8b900e221a
Dentstring <46445611+Dentstring@users.noreply.github.com>
2020-12-03 10:13:07 +0100

    Fixed remove functionality

--- WorldOfZuul/src/interactable/Shop.java
+++ WorldOfZuul/src/interactable/Shop.java
@@ -3,7 +3,7 @@ package interactable;
 public class Shop extends Interactable {
     public void removeItem(String word)    {
         for(int i = 0; i < this.getCommandList().getItems().size(); i++ )   {
-            if(this.getCommandList().getItems().get(i).equals("Buy " + word))    {
+            if(this.getCommandList().getItems().get(i).contains(word))    {
                 this.getCommandList().getItems().remove(i);
                 break;
             }
